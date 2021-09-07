a7a9ef804362f7096dd2f0c1d8866d7079d1d69d
Selim Horri <horri.slim@gmail.com>
2021-09-07 22:05:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ app.use(express.json());
 const movieRouter = require("./api/movie-api");
 app.use("/app/api", movieRouter);
 
-
-
-
-
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`app listening to port ${port} ...`));
 
